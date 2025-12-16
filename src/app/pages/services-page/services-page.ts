29b625559555d0d752e303d08be5ad25eedf8bcd
Anderson Rueda <andesondavid17@gmail.com>
2025-12-16 12:00:14 -0500

    feat: ajustes colores y tarjetas servicios

--- src/app/pages/services-page/services-page.ts
+++ src/app/pages/services-page/services-page.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { FooterService } from '../../services/footer.service';
+import { NavbarService } from '../../services/navbar.service';
 
 @Component({
   selector: 'app-services-page',
@@ -9,22 +10,40 @@ import { FooterService } from '../../services/footer.service';
   styleUrl: './services-page.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ServicesPage implements OnInit,OnDestroy {
+export class ServicesPage implements OnInit, OnDestroy {
   public cardToShow = signal(0);
   public footerService = inject(FooterService);
+  public navbarService = inject(NavbarService);
+
   public ngOnInit(): void {
     this.footerService.setFooterStyleToShow('small');
   }
   public ngOnDestroy(): void {
     this.footerService.setFooterStyleToShow('full');
+    this.navbarService.setNavbarStyleToShow('transparent');
   }
   public setCardToShow(card: number, event?: Event) {
     if (event) {
       event.stopPropagation();
     }
     this.cardToShow.set(card);
+    switch (card) {
+      case 1:
+        this.navbarService.setNavbarStyleToShow('text-white');
+        break;
+      case 2:
+        this.navbarService.setNavbarStyleToShow('text-white');
+        break;
+      case 3:
+        this.navbarService.setNavbarStyleToShow('text-white');
+        break;
+      default:
+        this.navbarService.setNavbarStyleToShow('text-white');
+        break;
+    }
   }
   closeModal() {
     this.cardToShow.set(0);
+    this.navbarService.setNavbarStyleToShow('transparent');
   }
 }
