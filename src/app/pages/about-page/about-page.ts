28069537af8bf64a985a7e4358529494201a3e91
Anderson Rueda <andesondavid17@gmail.com>
2026-01-19 11:18:16 -0500

    feat: correcion de estilos

--- src/app/pages/about-page/about-page.ts
+++ src/app/pages/about-page/about-page.ts
@@ -13,13 +13,13 @@ export class AboutPage {
 
   teamMembers = [
     {
-      name: 'MARÍA ALEJANDRA <br />RODRIGUEZ MOLINARES',
+      name: 'MARÍA ALEJANDRA RODRIGUEZ MOLINARES',
       roles: ['arquitecta', 'especialista en diseño de interiores', 'fundadora y directora creativa'],
       description: 'Apasionada por crear espacios que conecten con la esencia de quienes los habitan, combina su formación como arquitecta y su especialización en diseño de interiores con una sensibilidad especial por los detalles, la atmósfera y la funcionalidad. Disfruta diseñar mobiliario que aporte equilibrio y cuente historias dentro de cada proyecto, y lidera la dirección creativa para garantizar coherencia estética, calidez y un sello único en cada propuesta.',
       image: '/backgrounds/photo1.webp'
     },
     {
-      name: 'NELSON F. <br />ANNICHIARICO M.',
+      name: 'NELSON F. ANNICHIARICO M.',
       roles: ['arquitecto', 'fundador y', 'director de producción'],
       description: 'Experto en la materialización de ideas, lidera los procesos de producción asegurando que cada detalle técnico se ejecute con precisión. Su enfoque se centra en la calidad de los materiales y la excelencia en la construcción para garantizar resultados duraderos y estéticos.',
       image: '/backgrounds/photo1.webp' // Placeholder, user might want to change it later
